00e52e17b7d657277ed985d31a27512b461c7a47
Abdelaziz HAIMOUD <abdelazizhaimoud@outlook.com>
2025-01-15 09:30:16 +0100

    3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(` -  Server running on http://localhost:${PORT}`);
 });
 
 export default app;
